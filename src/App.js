e4dbeddc7fcc5d2c6fa381caecc1e6b754f13f91
rana-abhi777 <abhisekrana777@gmail.com>
2020-09-13 19:14:45 +0530

    Added Todo component with todo-form and todo-list display parts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import TodoList from './todos/TodoList';
 import './App.css';
 
 const App = () => (
   <div className="App">
-    <h1>Hello World of react!!!</h1>
-    <h2>This is trial for react hot reload.</h2>
+    <TodoList />
   </div>
 );
 
